68c0ed02b61d80a17370d937fdd5210e15c4678b
Nadim Nakhleh <nakhleh@gmail.com>
2021-06-09 21:54:40 -0400

    Java, Chapter 3

--- java/src/main/java/currency/Dollar.java
+++ java/src/main/java/currency/Dollar.java
@@ -8,6 +8,11 @@ class Dollar {
     }
 
     Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return new Dollar(this.amount * multiplier);
+    }
+
+    public boolean equals (Object object) {
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
     }
 }
--- java/src/test/java/currency/DollarTests.java
+++ java/src/test/java/currency/DollarTests.java
@@ -1,6 +1,9 @@
 package currency;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -13,5 +16,11 @@ public class DollarTests {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
