f5181efcf87f476e4cb13d88aaa245dd24679d34
Nadim Nakhleh <nakhleh@gmail.com>
2021-06-23 22:10:23 -0400

    xUnit, Chapter 20

--- py/src/pyunit.py
+++ py/src/pyunit.py
@@ -5,37 +5,32 @@ class TestCase:
     
     def setUp(self):
         pass
-
+    def tearDown(self):
+        pass
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
 
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
-
+        self.log = "setUp "
     def testMethod(self):
-        self.wasRun = 1
+        self.log += "testMethod "
+    def tearDown(self):
+        self.log += "tearDown"
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown" == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
 
 
 
