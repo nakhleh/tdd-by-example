e6a65a06ef9004096ffcb664a5f36dd7b7fe86b7
Nadim Nakhleh <nakhleh@gmail.com>
2021-06-23 21:37:15 -0400

    xUnit, Chapter 19

--- py/src/pyunit.py
+++ py/src/pyunit.py
@@ -14,24 +14,28 @@ class TestCase:
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
-        self.wasRun = None
 
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
-
 
     def testMethod(self):
         self.wasRun = 1
 
-
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
 
 
 
